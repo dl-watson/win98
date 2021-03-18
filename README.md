14ca1aa1fc6770882ae0772d33f4d25e646f9e8d
dl-watson <dlarkinwatson@gmail.com>
2021-03-18 11:51:05 -0700

    done for now

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 fix:
 
-- x's and \_'s close or minimize screens
+- \_'s close or minimize screens
 - clicking outside of the start menu maxes it minimize
-- possible to highlight text on a draggable window? double check, or remove the text cursor
-- notepad windows can be reopened from desktop shortcuts
 
 icons:
 https://win98icons.alexmeub.com/
--- app.js
+++ app.js
@@ -2,14 +2,31 @@ const ok = ".ok_button";
 const start_button = ".start_button";
 const start_menu = ".start_menu";
 const clock = "#showClock";
-const window_one = ".window-one";
-const window_two = ".window-two";
+const window_one = ".window_one";
+const window_two = ".window_two";
 const explorer = ".description";
+const note_one = ".note_one";
+const note_two = ".note_two";
+const window_one_x = ".window_one .x";
+const window_two_x = ".window_two .x";
+const explorer_x = ".description .x";
+const computer = ".computer";
+const contact = ".contact";
+const linkedin = ".pages";
+const github = ".code";
+const twitter = ".recycle";
 
 $(document).ready(() => {
   $(window_one).draggable();
   $(window_two).draggable();
+  $(note_one).draggable();
+  $(note_two).draggable();
   $(explorer).draggable();
+  $(computer).draggable();
+  $(contact).draggable();
+  $(linkedin).draggable();
+  $(github).draggable();
+  $(twitter).draggable();
 
   $(ok).click(() => {
     $(ok).parent().parent().fadeOut();
@@ -18,6 +35,30 @@ $(document).ready(() => {
   $(start_button).click(() => {
     $(start_button).toggleClass("sb_click");
     $(start_menu).slideToggle();
+  });
+
+  $(window_one_x).click(() => {
+    $(window_one).toggleClass("hidden");
+  });
+
+  $(window_two_x).click(() => {
+    $(window_two).toggleClass("hidden");
+  });
+
+  $(explorer_x).click(() => {
+    $(explorer).toggleClass("hidden");
+  });
+
+  $(note_one).click(() => {
+    if ($(window_one).hasClass("hidden")) {
+      $(window_one).toggleClass("hidden");
+    }
+  });
+
+  $(note_two).click(() => {
+    if ($(window_two).hasClass("hidden")) {
+      $(window_two).toggleClass("hidden");
+    }
   });
 
   const updateClock = () => {
