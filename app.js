a48d47020ac54c1c756d81d0abcd8808fe513151
dl-watson <dlarkinwatson@gmail.com>
2021-03-22 14:33:37 -0700

    debugging mobile display

--- app.js
+++ app.js
@@ -20,8 +20,4 @@ $(document).ready(() => {
 
   window.setInterval(updateClock, 6000);
   updateClock();
-
-  $(document).on("mobileinit", function () {
-    $.mobile.ignoreContentEnabled = true;
-  });
 });
